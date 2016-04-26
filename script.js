2a0cdb609fef97c001cdab61c620d81f2ab4c91c
raymondtieu <raymondtieu@users.noreply.github.com>
2016-04-26 12:09:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,171 @@
 $(document).ready(function() {
-	var t;
-	var tt;
+    var t;
 
-	let session = {
-		balance: parseFloat($("#balance").val()),
-		
-		total: 0,
-		wins: 0,
-		losses: 0,
+    let session = {
+        balance: parseFloat($("#balance").val()),
+        
+        total: 0,
+        wins: 0,
+        losses: 0,
 
-		totalBets: 0,
+        totalBets: 0,
 
-		history: [],
-	};
+        history: [],
+    };
 
-	let settings = {
-		multiplier: parseFloat($("#mult").val()),
-		winChance: 47.5,// win chance
-		speed: parseFloat($("#speed").val()),
-	};
+    let settings = {
+        winChance: parseFloat($("#chance").val()),
+        speed: parseFloat($("#speed").val()),
+        maxBet: parseFloat($("#maxBet").val()),
 
-	let round = {
-		roll: -1,
-		initial: 1,
-		bet: 1,
-		profit: 0,
-	};
+        winBase: false,
+        loseBase: false,
 
-	$("#start").click(function() {
-		$(this).prop( "disabled", true );
+        winMultiplier: 1,
+        loseMultiplier: 1,
+    };
 
-		tt = setInterval(function() {
-			if (round.bet > session.balance) {
-				clearInterval(tt);
-				clearInterval(t);
-				alert("Insufficient Funds");
-				$("#start").prop( "disabled", false );
-			}
-		}, 1);
+    let round = {
+        roll: -1,
+        initial: 1,
+        bet: 1
+    };
 
-		t = setInterval(function() { 
-			roll();
-		}, settings.speed);
-	});
+    $("#start").click(function() {
+        $(this).prop( "disabled", true );
 
-	$("#stop").click(function() {
-		clearInterval(t);
-		$("#start").prop( "disabled", false );
-	});
+        session.balance = parseFloat($("#balance").val());
 
-	function roll() {
-		session.totalBets++;
-		session.total += round.bet;
+        settings.speed = parseFloat($("#speed").val());
+        settings.winChance = parseFloat($("#chance").val());
+        settings.maxBet = parseFloat($("#maxBet").val());
 
-		round.roll = Math.random() * 100;
+        settings.winBase = $("#winReturnToBase").prop("checked");
+        settings.loseBase = $("#loseReturnToBase").prop("checked");
+        settings.winMultiplier = parseFloat($("#winMultiplierVal").val());
+        settings.loseMultiplier = parseFloat($("#loseMultiplierVal").val());
 
-		if (round.roll <= settings.winChance) {
-			onWin();
-		} else {
-			onLose();
-		}
-	}
+        console.log(settings);
 
-	function onWin() {
-		let winnings = round.bet*settings.multiplier
-		
-		session.balance+=winnings;
-		session.wins+=winnings;
+        round.initial = parseFloat($("#bet").val());
+        round.bet = round.initial;
 
-		round.bet = round.initial;
+        t = setInterval(function() { 
+            roll();
+        }, settings.speed);
 
-		let views = [];
-		views.push(round.roll);
-		views.push("<strong style='color:green;'>" + winnings + "</strong>");
+        $(".setting").each((i, e) => {
+            $(e).prop("disabled", true);
+        });
+        $("#start").addClass("disabled");
+    });
 
-		updateView(views);
+    $("#stop").click(function() {
+        clearInterval(t);
+        $("#start").prop( "disabled", false );
+        $("#start").removeClass("disabled");
 
-	}
+        $(".setting").each((i, e) => {
+            $(e).prop("disabled", false);
+        });
+    });
 
-	function onLose() {
-		session.balance-=round.bet;
-		session.losses+=round.bet;
+    function roll() {
+        if (round.bet > session.balance) {
+            clearInterval(t);
+            alert("Insufficient Funds");
 
-		round.bet*=2;
+            $("#stop").click();
+            return;
+        }
 
-		let views = [];
-		views.push(round.roll);
-		views.push("<strong style='color:red;'>" + round.bet + "</strong>");
+        if (round.bet > settings.maxBet) {
+            clearInterval(t);
+            alert("Max bet is reached");
 
-		updateView(views);
-	}
+            $("#stop").click();
+            return;
+        }
 
-	function updateView(views) {
-		// Update history of rolls
-		session.history.push(views);
+        session.totalBets++;
+        session.total += round.bet;
 
-		$("#t").html("");
-		session.history.forEach((row) => {
-			$("#t").append("<tr><td>" + row[0] + "</td><td>" + row[1] + "</tr>");
-		});
+        round.roll = Math.random() * 100;
 
-		if (session.history.length > 15) {
-			session.history.shift();
-		}
+        if (round.roll <= settings.winChance) {
+            onWin();
+        } else {
+            onLose();
+        }
+    }
 
-		// Update session
-		$("#balance").val(session.balance);
-		$("#profit").val(session.wins - session.losses);
-		$("#num").val(session.totalBets);
-		$("#total").val(session.total);
-		$("#win").val(session.wins);
-		$("#lose").val(session.losses);
-		$("#bet").val(round.bet);
-	}
+    function onWin() {
+        //TODO
+        let payout = 1;
+
+        let winnings = round.bet * payout;
+        
+        session.balance+=winnings;
+        session.wins+=winnings;
+
+        let views = [];
+        views.push(round.roll);
+        views.push("<strong style='color:green;'>" + winnings + "</strong>");
+
+        updateView(views);
+
+        if (settings.winBase) {
+            round.bet = round.initial;
+        } else {
+            round.bet *= settings.winMultiplier;
+        }
+    }
+
+    function onLose() {
+        session.balance-=round.bet;
+        session.losses+=round.bet;
+
+        let views = [];
+        views.push(round.roll);
+        views.push("<strong style='color:red;'>" + round.bet + "</strong>");
+
+        updateView(views);
+
+        if (settings.loseBase) {
+            round.bet = round.initial;
+        } else {
+            round.bet *= settings.loseMultiplier;
+        }
+    }
+
+    function updateView(views) {
+        // Update history of rolls
+        session.history.push(views);
+
+        $("#t").html("");
+        session.history.forEach((row) => {
+            $("#t").append("<tr><td>" + row[0] + "</td><td>" + row[1] + "</tr>");
+        });
+
+        if (session.history.length > 15) {
+            session.history.shift();
+        }
+
+        // Update session
+        $("#balance").val(session.balance);
+        $("#profit").val(session.wins - session.losses);
+        $("#num").val(session.totalBets);
+        $("#total").val(session.total);
+        $("#win").val(session.wins);
+        $("#lose").val(session.losses);
+        $("#bet").val(round.bet);
+    }
 });
+
+function sliderChange(val) {
+    $("#chance").val(val);
+}
+
+function chanceChange(val) {
+    $("#slider").val(val);
+}
